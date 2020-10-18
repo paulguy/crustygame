7280805b9f02eaaa75bceb792e42e486687e8e88
Paul LaMendola <paulguy119@gmail.com>
2020-10-17 20:28:48 -0400

    I guess properly license the various files.
    The net stuff isn't written by me so currently it's separately licensed,
    I guess.  It's not integrated in to the main program yet, so that
    shouldn't be a problem at the moment...

--- callbacks.h
+++ callbacks.h
@@ -1,2 +1,23 @@
+/*
+ * Copyright 2020 paulguy <paulguy119@gmail.com>
+ *
+ * This file is part of crustygame.
+ *
+ * crustygame is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * crustygame is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with crustygame.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* not sure this file needs a license but sure */
+
 extern CrustyCallback cb[];
 extern const int CRUSTYGAME_CALLBACKS; 
